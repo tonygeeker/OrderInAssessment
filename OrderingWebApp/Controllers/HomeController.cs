87e9b4526e9e192a162af6301ee7ef544d2a246f
Tony <tonysilangwe@gmail.com>
2020-11-04 16:54:30 +0200

    [add] search feature front end: back end integration

--- OrderingWebApp/Controllers/HomeController.cs
+++ OrderingWebApp/Controllers/HomeController.cs
@@ -2,36 +2,46 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using OrderingWebApp.Models;
 
 namespace OrderingWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private IConfiguration _config { get; set; }
+
+        public HomeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(new SearchViewModel());
         }
 
-        public IActionResult About()
+       public async Task<IActionResult> Search(SearchViewModel model)
         {
-            ViewData["Message"] = "Your application description page.";
+            string baseURL = _config.GetValue<string>("MySettings:baseURL");
+            var requestUri = $"{baseURL}api/foodSearch?foodSearch={model.FoodItem}&location={model.Location}";
+            using (var client = new HttpClient())
+            using (var response = await client.GetAsync(requestUri))
+            using (var content = response.Content)
+            {
+                string data = await content.ReadAsStringAsync();
+                if (data == null)
+                {
+                    return View(new List<RestuarantViewModel>());
+                }
 
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
+                var matchingShops = JsonConvert.DeserializeObject<List<RestuarantViewModel>>(data);
+                return View(matchingShops);
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
